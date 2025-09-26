d29e95276a7bc83fc09eeda61b8a2529f79d99b6
Artem Sinitsa <artemsss009@gmail.com>
2025-09-26 11:43:26 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,37 +1,21 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"os"
 )
 
-//cd D:\MyProjects\TelegramBot\tg_todolist_bot
-//dir
-//go mod tidy
-//go build -o bot.exe
-//.\bot.exe -tg-bot-token "7982922881:AAGDbLHmt3csIbianT_j1PJ-83jBzWjX07g"
-
 func main() {
-	// флаг для токена
-	tgTokenFlag := flag.String("tg-bot-token", "", "Telegram bot token")
-	flag.Parse()
-
-	// 1) сначала смотрим флаг, 2) если пустой — переменная окружения
-	token := *tgTokenFlag
+	token := os.Getenv("TELEGRAM_TOKEN")
 	if token == "" {
-		token = os.Getenv("TELEGRAM_BOT_TOKEN")
-	}
-
-	if token == "" {
-		log.Fatal("bot token not provided: use -tg-bot-token or set TELEGRAM_BOT_TOKEN")
+		log.Fatal("TELEGRAM_TOKEN не установлен")
 	}
 
 	bot, err := NewBot(token, "data")
 	if err != nil {
-		log.Fatalf("failed to create bot: %v", err)
+		log.Fatalf("Ошибка запуска бота: %v", err)
 	}
 
-	log.Println("Bot started")
+	log.Printf("Authorized on account %s", bot.api.Self.UserName)
 	bot.Run()
 }
